65f4b73a7a4f4708cc5028088cc14d336fa3dd7b
Lars-Petter Helland <lars-petter.helland@hvl.no>
2021-02-22 15:20:13 +0100

    Det jeg gjorde mens dere holdt på. Får ikke den siste testen til å passere. Kan noen se hva som er feil?

--- lab-cache-for-yr-using-mockito/src/main/java/myapp/cache/ForecastCache.java
+++ lab-cache-for-yr-using-mockito/src/main/java/myapp/cache/ForecastCache.java
@@ -1,12 +1,31 @@
 package myapp.cache;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import myapp.domain.Forecast;
+import myapp.domain.Timestamp;
 
 public class ForecastCache {
 	
-	public Forecast getForecastFor(String location) {
-		return null;
+	private ForecastFetcher forecastFetcher;
+	private TimestampService timestampService;
+	
+	private Map<String, Forecast> cache = new HashMap<>();
+	
+	public ForecastCache(ForecastFetcher forecastFetcher,
+			TimestampService timestampService) {
+		this.forecastFetcher = forecastFetcher;
+		this.timestampService = timestampService;
 	}
 
-
+	public Forecast getForecastFor(String location) {
+		Forecast f = cache.get(location);
+		if (f == null || timestampService.hasExpired(f.getTimestamp())) {
+			f = new Forecast(location);
+			f.setTimestamp(new Timestamp("", ""));
+			cache.put(location, forecastFetcher.fetchForecastFor(location));
+		}
+		return cache.get(location);
+	}
 }
--- lab-cache-for-yr-using-mockito/src/test/java/myapp/cache/ForecastCacheTest.java
+++ lab-cache-for-yr-using-mockito/src/test/java/myapp/cache/ForecastCacheTest.java
@@ -1,8 +1,6 @@
 package myapp.cache;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -26,6 +24,12 @@ public class ForecastCacheTest {
 
 	// --- Data values used in the tests -------------------------------------
 	
+	private static final String BERGEN = "Bergen";
+	private static final String OSLO = "Oslo";
+	private static final Forecast FORECAST1_FROM_BERGEN = new Forecast(BERGEN);
+	private static final Forecast FORECAST2_FROM_BERGEN = new Forecast(BERGEN);
+	private static final Forecast FORECAST_FROM_OSLO = new Forecast(OSLO);
+	
 		// ... Maybe you can set up some test data to be reused in the tests?
 
 
@@ -42,18 +46,69 @@ public class ForecastCacheTest {
 	public void setup() {
 		
 		// ... General setup ...
+		forecastCache = new ForecastCache(
+				mockedForecastFetcher, mockedTimestampService);
 		
 		// ... Maybe you eventually can refactor out default behavior for the 
 		//     mocks, which can be overridden in test methods if needed?
+		when(mockedForecastFetcher.fetchForecastFor(BERGEN))
+				.thenReturn(FORECAST1_FROM_BERGEN).thenReturn(FORECAST2_FROM_BERGEN);
+		when(mockedForecastFetcher.fetchForecastFor(OSLO))
+				.thenReturn(FORECAST_FROM_OSLO);
+		when(mockedTimestampService.hasExpired(any(Timestamp.class)))
+				.thenReturn(false);
 	}
 
 	// --- The tests ---------------------------------------------------------
 
 	@Test
 	public void firstRequestShouldFetchAndReturnForcastFromYr() {
-		//fail();
+		
+		assertSame(FORECAST1_FROM_BERGEN, forecastCache.getForecastFor(BERGEN));
+		verify(mockedForecastFetcher, times(1)).fetchForecastFor(BERGEN);
 	}
 	
-	// ... more tests ...
+	
+	@Test
+	public void secondRequestShouldReturnForcastFromCache() {
+		
+		//Fetching one forecast for Bergen and storing in cache
+		forecastCache.getForecastFor(BERGEN);
+		
+		assertSame(FORECAST1_FROM_BERGEN, forecastCache.getForecastFor(BERGEN));
+		verify(mockedForecastFetcher, times(1)).fetchForecastFor(BERGEN);
+	}
 
+	@Test
+	public void firstRequestToOtherLocationShouldFetch() {
+		
+		//Fetching one forecast for Bergen and storing in cache
+		forecastCache.getForecastFor(BERGEN);
+
+		assertSame(FORECAST_FROM_OSLO, forecastCache.getForecastFor(OSLO));
+		verify(mockedForecastFetcher, times(1)).fetchForecastFor(OSLO);
+	}
+	
+	@Test
+	public void secondRequestWithExpiredTimestampBlabla() {
+		
+		//Fetching one forecast for Bergen and storing in cache
+		forecastCache.getForecastFor(BERGEN);
+		
+		when(mockedTimestampService.hasExpired(any(Timestamp.class)))
+				.thenReturn(true);
+		
+		assertSame(FORECAST2_FROM_BERGEN, forecastCache.getForecastFor(BERGEN));
+		verify(mockedForecastFetcher, times(2)).fetchForecastFor(BERGEN);
+	}
+
+	
 }
+
+
+
+
+
+
+
+
